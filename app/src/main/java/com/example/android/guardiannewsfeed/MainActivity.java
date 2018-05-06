d4fc419bb82c1841416513162913a89ccc6bf0d7
tr0lczyk <matt.olczyk@gmail.com>
2018-05-07 00:10:21 +0200

    creataion of the repository and the basic files template

--- app/src/main/java/com/example/android/guardiannewsfeed/MainActivity.java
+++ app/src/main/java/com/example/android/guardiannewsfeed/MainActivity.java
@@ -7,6 +7,8 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String LOG = MainActivity.class.getName();
 
+    public static final String URL_LINK = "http://content.guardianapis.com/search?q=harry%20potter&api-key=57228fac-f4ff-4f95-8096-0acf0f9ca0db";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
